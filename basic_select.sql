ea3de1011e202c25e053705e6c195f6234c06d80
redfords <joanapiovaroli@gmail.com>
2021-03-23 18:46:00 -0300

    Update basic_select.sql

--- basic_select.sql
+++ basic_select.sql
@@ -1,9 +1,7 @@
-/*
-Query all columns for all American cities in the CITY table with populations larger than 100000.
-The CountryCode for America is USA.
-*/
+-- Query all columns for all American cities in the CITY table with populations larger than 100000.
+-- The CountryCode for America is USA.
 
-SELECT * FROM CITY
-WHERE countrycode = 'USA' AND
+select * from CITY
+where countrycode = 'USA' and
 population > 100000
 
