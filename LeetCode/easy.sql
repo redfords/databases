1a95c914ec2a5ecfccdae9fec568f12fa415f963
redfords <joanapiovaroli@gmail.com>
2021-03-24 00:45:15 -0300

    Update easy.sql

--- LeetCode/easy.sql
+++ LeetCode/easy.sql
@@ -79,4 +79,29 @@ left join (
     a.email = b.email
 where b.id is null
 
+/* Rising Temperature
+Write an SQL query to find all dates' id with higher temperature compared to its previous dates
+(yesterday). Return the result table in any order. */
 
+select a.id
+from weather a, weather b
+where datediff(a.recorddate, b.recorddate) = 1 and
+a.temperature > b.temperature
+
+/* Classes More Than 5 Students
+There is a table courses with columns: student and class. List all classes which have more than
+or equal to 5 students. */
+
+select class
+from courses
+group by class
+having count(distinct student) > 4
+
+/* Second Highest Salary
+Write a SQL query to get the second highest salary from the Employee table. */
+
+select max(salary) as SecondHighestSalary
+from employee
+where salary < (
+    select max(salary)
+    from employee)
