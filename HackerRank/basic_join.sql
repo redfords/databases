0b21b718a6481d3155d349b9c9591e3759ecf905
redfords <joanapiovaroli@gmail.com>
2021-04-27 20:09:12 -0300

    Update basic_join.sql

--- HackerRank/basic_join.sql
+++ HackerRank/basic_join.sql
@@ -49,7 +49,17 @@ buy each non-evil wand of high power and age. Write a query to print the id, age
 power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand
 has same power, sort the result in order of descending age.*/
 
-
+select id, age, m.coins_needed, m.power
+from
+    (select code, power, MIN(coins_needed) AS coins_needed
+    from Wands
+    group by code, power) as m
+join Wands as w
+on m.code = w.code and m.power = w.power and m.coins_needed = w.coins_needed
+join Wands_Property as wp
+on m.code = wp.code
+where wp.is_evil = 0
+order by m.power desc, age desc
 
 /* Challenges
 Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name,
@@ -58,7 +68,23 @@ challenges in descending order. If more than one student created the same number
 sort the result by hacker_id. If more than one student created the same number of challenges and the
 count is less than the maximum number of challenges created, then exclude those students. */
 
-
+select c.hacker_id, h.name, count(c.challenge_id) as challenges_created
+from Hackers h
+join Challenges c
+on h.hacker_id = c.hacker_id
+group by c.hacker_id,  h.name
+having challenges_created =
+    (select count(c1.challenge_id)
+    from Challenges c1
+    group by c1.hacker_id
+    order by count(*) desc
+    limit 1)
+or challenges_created not in
+    (select count(c2.challenge_id)
+    from Challenges c2
+    group by c2.hacker_id
+    having c2.hacker_id <> c.hacker_id)
+order by challenges_created desc, c.hacker_id
 
 /* Contest Leaderboard
 You did such a great job helping Julia with her last coding contest challenge that she wants you to work
@@ -67,3 +93,12 @@ challenges. Write a query to print the hacker_id, name, and total score of the h
 descending score. If more than one hacker achieved the same total score, then sort the result by
 ascending hacker_id. Exclude all hackers with a total score of  from your result. */
 
+select m.hacker_id, h.name, sum(m.score) as total_score
+from (select hacker_id, challenge_id, max(score) as score
+    from Submissions
+    group by hacker_id, challenge_id) as m
+join Hackers h
+on h.hacker_id = m.hacker_id
+group by m.hacker_id, h.name
+having total_score > 0
+order by total_score desc, h.hacker_id
