befa4c33d6c6a80e893b5fa0e9e878ef049e97e7
redfords <joanapiovaroli@gmail.com>
2021-04-29 22:06:45 -0300

    Update advanced_join.sql

---
+++ HackerRank/advanced_join.sql
@@ -0,0 +1,10 @@
+/* SQL Project Planning
+You are given a table, Projects, containing three columns: Task_ID, Start_Date and End_Date. It is
+guaranteed that the difference between the End_Date and the Start_Date is equal to 1 day for each row
+in the table.
+If the End_Date of the tasks are consecutive, then they are part of the same project. Samantha is
+interested in finding the total number of different projects completed.
+Write a query to output the start and end dates of projects listed by the number of days it took to
+complete the project in ascending order. If there is more than one project that have the same number
+of completion days, then order by the start date of the project.*/
+
