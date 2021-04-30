564718ee078f4fcacdaa945116538721262d8ea1
redfords <joanapiovaroli@gmail.com>
2021-04-29 21:53:17 -0300

    Update advanced_select.sql

--- HackerRank/advanced_select.sql
+++ HackerRank/advanced_select.sql
@@ -15,3 +15,21 @@ if(a + b > c and a + c > b and b + c > a,
 from triangles
 
 
+/* The PADS
+Generate the following two result sets:
+1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first
+letter of each profession as a parenthetical (i.e.: enclosed in parentheses).
+2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending
+order, and output them in the following format:
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation]
+is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they
+should be ordered alphabetically.*/
+
+select concat(name, '(', left(occupation, 1), ')')
+from occupations
+order by name;
+select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.')
+from occupations
+group by occupation
+order by count(occupation), occupation
