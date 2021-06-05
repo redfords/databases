4b3d2e80cb988efa5999c138b13121e167888bec
redfords <joanapiovaroli@gmail.com>
2021-06-05 00:31:56 -0300

    Add all customers and products

--- HackerRank/advanced_join.sql
+++ HackerRank/advanced_join.sql
@@ -8,3 +8,38 @@ Write a query to output the start and end dates of projects listed by the number
 complete the project in ascending order. If there is more than one project that have the same number
 of completion days, then order by the start date of the project.*/
 
+/*
+Sales Data for All Customers and Products
+Write a query that returns sales details of all customers and products. The query should return all
+customers, even customers without invoices and also all products, even those products that were not
+sold. Print "N/A" for a null customer or product name, and 0 for a null quantity.
+
+For each row return customer name, product name and the quantity of the product sold. Order the
+result ascending by customer id, product id and invoice item id.
+*/
+
+(select
+    coalesce(customer_name, "N/A") as customer_name,
+    coalesce(product_name, "N/A") as product_name,
+    coalesce(quantity, 0) as quantity
+from customer
+left join invoice
+on customer.id = invoice.customer_id
+left join invoice_item
+on invoice.id = invoice_item.invoice_id
+left join product
+on product.id = invoice_item.product_id
+order by customer.id, product.id, invoice_item.id)
+union
+(select
+    coalesce(customer_name, "N/A") as customer_name,
+    coalesce(product_name, "N/A") as product_name,
+    coalesce(quantity, 0) as quantity
+from customer
+inner join invoice
+on customer.id = invoice.customer_id
+inner join invoice_item
+on invoice.id = invoice_item.invoice_id
+right join product
+on product.id = invoice_item.product_id
+order by customer.id, product.id, invoice_item.id)
