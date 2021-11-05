9986b857d8b9196f757952bb3cfb63d459b4ef75
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-11-04 22:40:47 -0300

    Create easy.sql

---
+++ StrataScratch/easy.sql
@@ -0,0 +1,4 @@
+/* Salaries Differences
+Write a query that calculates the difference between the highest salaries found in the marketing
+and engineering departments. Output just the difference in salaries.
+Tables: db_employee, db_dept */
