94573f7041ec79d7a30b449ff8ca84dd8fdd6f87
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-12-26 23:56:49 -0300

    Add union distinct

--- StrataScratch/kaggle.sql
+++ StrataScratch/kaggle.sql
@@ -12,4 +12,14 @@ cannot take the UNION of the Age column from the owners table and the Pet_Name c
 We use UNION ALL to include duplicate values - you'll notice that 9 appears in both the owners table and the pets table, and shows up
 twice in the concatenated results. If you'd like to drop duplicate values, you need only change UNION ALL in the query to UNION DISTINCT.
 
+# Query to select all users who posted stories or comments on January 1, 2014
+union_query = """
+              SELECT c.by
+              FROM `bigquery-public-data.hacker_news.comments` AS c
+              WHERE EXTRACT(DATE FROM c.time_ts) = '2014-01-01'
+              UNION DISTINCT
+              SELECT s.by
+              FROM `bigquery-public-data.hacker_news.stories` AS s
+              WHERE EXTRACT(DATE FROM s.time_ts) = '2014-01-01'
+              """
 */
