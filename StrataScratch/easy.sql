0ad654bfbd1c703680bcad0bb1ee475b39e2b9dd
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-12-28 23:30:56 -0300

    Update easy.sql

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -1,4 +1,29 @@
-/* Salaries Differences
+/*
+Salaries Differences
 Write a query that calculates the difference between the highest salaries found in the marketing
 and engineering departments. Output just the difference in salaries.
-Tables: db_employee, db_dept */
+Tables: db_employee, db_dept
+*/
+
+
+
+/*
+Popularity of Hack
+Facebook has developed a new programing language called Hack.To measure the popularity of Hack they
+ran a survey with their employees. The survey included data on previous programing familiarity as well
+as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to
+an error location data was not collected, but your supervisor demands a report showing average popularity
+of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
+Based on the above, find the average popularity of the Hack per office location.
+Output the location along with the average popularity.
+*/
+
+
+
+/*
+Finding Updated Records
+We have a table with employees and their salaries, however, some of the records are old and contain
+outdated salary information. Find the current salary of each employee assuming that salaries increase each
+year. Output their id, first name, last name, department ID, and current salary. Order your list by employee
+ID in ascending order.
+*/
