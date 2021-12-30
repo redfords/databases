b6b9b8aae7d0891df204b6377b176976112ec1cb
Joana Piovaroli <joanapiovaroli@gmail.com>
2021-12-29 23:57:53 -0300

    Update easy.sql

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -46,7 +46,13 @@ Compare each employee's salary with the average salary of the corresponding depa
 Output the department, first name, and salary of employees along with the average salary of that department.
 */
 
-/**/
+
+
+/*
+Total Cost Of Orders
+Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost.
+Order records by customer's first name alphabetically.
+*/
 
 /**/
 
