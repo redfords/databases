2cecbfbb525e000f258cbd0cfd5e301ef7a36691
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-03-28 23:57:02 -0300

    Add daily active users

--- YouTube/spotify.sql
+++ YouTube/spotify.sql
@@ -1,1 +1,10 @@
+/* Source: https://www.youtube.com/watch?v=-YdAIMjHZrM */
 
+/*
+Question 1: find total active users each day
+event_date  total_active_users
+2022-01-01  3
+2022-01-02  1
+2022-01-03  3
+2022-01-04  1
+*/
