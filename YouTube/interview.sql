f1c30061d9889a60efde98b9ae2d4dad9d5b010e
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-03-29 16:35:59 -0300

    Add comments

--- YouTube/interview.sql
+++ YouTube/interview.sql
@@ -3,8 +3,8 @@
 -- Write a SQL query to count the number of unique users per day who logged in from both iPhone and web,
 -- where iPhone logs and web logs are in distinct relations.
 
-iphone: ts | userid | iphone_sessionid
-web: ts | userid | web_sessionid
+-- iphone: ts | userid | iphone_sessionid
+-- web: ts | userid | web_sessionid
 
 
 /* Source: https://www.youtube.com/watch?v=n9OTXvYaP5k */
