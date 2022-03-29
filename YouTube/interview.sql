9feb42e05aea58ecffc573984585ab302b15b985
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-03-29 16:34:23 -0300

    Add friend acceptance rate

--- YouTube/interview.sql
+++ YouTube/interview.sql
@@ -1,8 +1,14 @@
-/* Srouce: https://www.youtube.com/watch?v=Td-cmLfQ7uU */
+/* Source: https://www.youtube.com/watch?v=Td-cmLfQ7uU */
 
 -- Write a SQL query to count the number of unique users per day who logged in from both iPhone and web,
 -- where iPhone logs and web logs are in distinct relations.
 
 iphone: ts | userid | iphone_sessionid
 web: ts | userid | web_sessionid
+
+
+/* Source: https://www.youtube.com/watch?v=n9OTXvYaP5k */
+
+-- Write a SQL query to find out the overall friend acceptance rate for each day.
+-- friend_requests: ds | sender | receiver | action (sent, accepted, rejected etc)
 
