cac44afdb910d294c33a933484c6c61a089b8a2e
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-03-29 16:31:25 -0300

    Add unique users iPhone and web

--- YouTube/interview.sql
+++ YouTube/interview.sql
@@ -1,1 +1,8 @@
 /* Srouce: https://www.youtube.com/watch?v=Td-cmLfQ7uU */
+
+-- Write a SQL query to count the number of unique users per day who logged in from both iPhone and web,
+-- where iPhone logs and web logs are in distinct relations.
+
+iphone: ts | userid | iphone_sessionid
+web: ts | userid | web_sessionid
+
