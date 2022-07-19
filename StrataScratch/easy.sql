e4acc969763b6ca6a8261509c6a670fa3a92361d
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-19 01:05:56 -0300

    Add Finding Updated Records

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -1,63 +1,34 @@
 /*
-Salaries Differences
-Write a query that calculates the difference between the highest salaries found in the marketing
-and engineering departments. Output just the difference in salaries.
-Tables: db_employee, db_dept
-*/
-
-
-
-/*
-Popularity of Hack
-Facebook has developed a new programing language called Hack.To measure the popularity of Hack they
-ran a survey with their employees. The survey included data on previous programing familiarity as well
-as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to
-an error location data was not collected, but your supervisor demands a report showing average popularity
-of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
-Based on the above, find the average popularity of the Hack per office location.
-Output the location along with the average popularity.
-*/
-
-
-
-/*
 Finding Updated Records
-We have a table with employees and their salaries, however, some of the records are old and contain
-outdated salary information. Find the current salary of each employee assuming that salaries increase each
-year. Output their id, first name, last name, department ID, and current salary. Order your list by employee
-ID in ascending order.
-*/
 
+We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information.
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name,
+department ID, and current salary. Order your list by employee ID in ascending order.
 
+ms_employee_salary
+id: int
+first_name: varchar
+last_name: varchar
+salary: int
+department_id: int
 
-/*
-Customer Details
-Find the details of each customer regardless of whether the customer made an order. Output the customer's
-first name, last name, and the city along with the order details.
-You may have duplicate rows in your results due to a customer ordering several of the same items. Sort
-records based on the customer's first name and the order details in ascending order.
-*/
-
-
-
-/*
-Average Salaries
-Compare each employee's salary with the average salary of the corresponding department.
-Output the department, first name, and salary of employees along with the average salary of that department.
-*/
-
-
-
-/*
-Total Cost Of Orders
-Find the total cost of each customer's orders. Output customer's id, first name, and the total order cost.
-Order records by customer's first name alphabetically.
-*/
-
-
-
-/*
-Number Of Bathrooms And Bedrooms
-Find the average number of bathrooms and bedrooms for each city’s property types. Output the result along
-with the city name and the property type.
+with max_salary as (
+  select
+  id,
+  first_name,
+  last_name,
+  department_id,
+  salary,
+  rank () over (partition by id order by salary desc) as rank
+  from ms_employee_salary )
+select
+id,
+first_name,
+last_name,
+department_id,
+salary
+from max_salary
+where rank = 1
+order by id asc	
+;
 */
