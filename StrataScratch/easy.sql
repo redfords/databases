405041381f354b4db22bfc3fa7e96f92f0648e80
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-19 01:11:51 -0300

    Add Find the base pay for Police Captains

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -32,3 +32,31 @@ from max_salary
 where rank = 1
 order by id asc;
 
+
+/*
+Find the base pay for Police Captains
+
+Output the employee name along with the corresponding base pay.
+
+sf_public_salaries
+id: int
+employeename: varchar
+jobtitle: varchar
+basepay: float
+overtimepay: float
+otherpay: float
+benefits: float
+totalpay: float
+totalpaybenefits: float
+year: int
+notes: datetime
+agency: varchar
+status: varchar
+*/
+
+select
+employeename,
+basepay
+from sf_public_salaries
+where jobtitle like '%CAPTAIN%POLICE%';
+
