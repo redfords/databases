654daaa29ad7fcc1a77f83ec54fc00249ab12f58
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-20 00:48:17 -0300

    Add Customer Details

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -117,3 +117,39 @@ from airbnb_search_details
 group by
 property_type,
 city;
+
+/*
+Customer Details
+
+Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name,
+and the city along with the order details.
+You may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the
+customer's first name and the order details in ascending order.
+
+customers
+id: int
+first_name: varchar
+last_name: varchar
+city: varchar
+address: varchar
+phone_number: varchar
+
+orders
+id: int
+cust_id: int
+order_date: datetime
+order_details: varchar
+total_order_cost: int
+*/
+
+select
+first_name,
+last_name,
+city,
+order_details
+from customers
+left join orders
+on customers.id = orders.cust_id
+order by
+first_name,
+order_details;
