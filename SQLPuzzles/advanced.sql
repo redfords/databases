a62a7062bd297bf93b9e03df643dfe72d89f3102
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-20 03:05:03 -0300

    Create advanced.sql

---
+++ SQLPuzzles/advanced.sql
@@ -0,0 +1,195 @@
+/*
+source: https://advancedsqlpuzzles.com/
+*/
+
+/*
+1. Shopping Carts
+*/
+
+/*
+2. Managers and Employees
+*/
+
+/*
+3. Fiscal Year Pay Rates
+*/
+
+/*
+4. Two Predicates
+*/
+
+/*
+5. Phone Directory
+*/
+
+/*
+6. Workflow Steps
+*/
+
+/*
+7. Mission to Mars
+*/
+
+/*
+8. Workflow Cases
+*/
+
+/*
+9. Matching Sets
+*/
+
+/*
+10. Mean, Median, Mode, and Range
+*/
+
+/*
+11. Permutations
+*/
+
+/*
+12. Average Days
+*/
+
+/*
+13. Inventory Tracking
+*/
+
+/*
+14. Indeterminate Process Log
+*/
+
+/*
+15. Group Concatenation
+*/
+
+/*
+16. Reciprocals
+*/
+
+/*
+17. De-Grouping
+*/
+
+/*
+18. Seating Chart
+*/
+
+/*
+19. Back to the Future
+*/
+
+/*
+20. Price Points
+*/
+
+/*
+21. Average Monthly Sales
+*/
+
+/*
+22. Occurrences
+*/
+
+/*
+23. Divide in Half
+*/
+
+/*
+24. Page Views
+*/
+
+/*
+25. Top Vendors
+*/
+
+/*
+26. Previous Year's Sales
+*/
+
+/*
+27. Delete the Duplicates
+*/
+
+/*
+28. Fill the Gaps
+*/
+
+/*
+29. Count the Groupings
+*/
+
+/*
+30. Select Star
+*/
+
+/*
+31. Second Highest
+*/
+
+/*
+32. First and Last
+*/
+
+/*
+33. Deadlines
+*/
+
+/*
+34. Specific Exclusion
+*/
+
+/*
+35. International vs Domestic Sales
+*/
+
+/*
+36. Traveling Salesman
+*/
+
+/*
+37. Group Criteria Keys
+*/
+
+/*
+38. Reporting Elements
+*/
+
+/*
+39. Prime Numbers
+*/
+
+/*
+40. Sort Order
+*/
+
+/*
+41. Associate IDs
+*/
+
+/*
+42. Mutual Friends
+*/
+
+/*
+43. Unbounded Preceding
+*/
+
+/*
+44. Slowly Changing Dimension Part I
+*/
+
+/*
+45. Slowly Changing Dimension Part II
+*/
+
+/*
+46. Negative Account Balances
+*/
+
+/*
+47. Work Schedule
+*/
+
+/*
+48. Consecutive Sales
+*/
