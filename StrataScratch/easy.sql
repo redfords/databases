e76ed2f600f680970cfd0a02b5790d5a6859a7fb
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-21 02:16:21 -0300

    Add Order Details

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -153,3 +153,39 @@ on customers.id = orders.cust_id
 order by
 first_name,
 order_details;
+
+/*
+Order Details
+
+Find order details made by Jill and Eva.
+Consider the Jill and Eva as first names of customers.
+Output the order date, details and cost along with the first name.
+Order records based on the customer id in ascending order.
+
+customers
+id: int
+first_name: varchar
+last_name: varchar
+city: varchar
+address: varchar
+phone_number: varchar
+
+orders
+id: int
+cust_id: int
+order_date: datetime
+order_details: varchar
+total_order_cost: int
+*/
+
+select
+first_name,
+order_date,
+order_details,
+total_order_cost
+from customers
+inner join orders
+on customers.id = orders.cust_id
+where first_name = 'Eva' or
+first_name = 'Jill'
+order by cust_id;
