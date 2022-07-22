52b9c5deb4b8c281c1834b475b4511114b6c050a
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-22 15:31:41 -0300

    Add Number of violations

--- StrataScratch/medium.sql
+++ StrataScratch/medium.sql
@@ -23,3 +23,37 @@ where cool = (
   select max(cool)
   from yelp_reviews
   );
+
+/*
+Number of violations
+
+You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe' for each year. If an
+inspection resulted in a violation, there will be a value in the 'violation_id' column. Output the number of violations by year in
+ascending order.
+
+sf_restaurant_health_violations
+business_id: int
+business_name: varchar
+business_address: varchar
+business_city: varchar
+business_state: varchar
+business_postal_code: float
+business_latitude: float
+business_longitude: float
+business_location: varchar
+business_phone_number: float
+inspection_id: varchar
+inspection_date: datetime
+inspection_score: float
+inspection_type: varchar
+violation_id: varchar
+violation_description: varchar
+risk_category: varchar
+*/
+
+select
+  date_part('year', inspection_date) as year,
+  count(inspection_id) as n_inspections
+from sf_restaurant_health_violations
+where business_name = 'Roxanne Cafe'
+group by year;
