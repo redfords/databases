acd8d28dfc05bdb8f1808cfb7d909d99b2d68bf2
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-22 12:13:04 -0300

    Add Bikes Last Used

--- StrataScratch/easy.sql
+++ StrataScratch/easy.sql
@@ -312,3 +312,30 @@ select
   max(max_salary) -
   min(max_salary)
 from employee_dept;
+
+/*
+Bikes Last Used
+
+Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time
+the bike was returned). Order the results by bikes that were most recently used.
+
+dc_bikeshare_q1_2012
+duration: varchar
+duration_seconds: int
+start_time: datetime
+start_station: varchar
+start_terminal: int
+end_time: datetime
+end_station: varchar
+end_terminal: int
+bike_number: varchar
+rider_type: varchar
+id: int
+*/
+
+select
+  bike_number,
+  max(end_time) as last_used
+from dc_bikeshare_q1_2012
+group by bike_number
+order by last_used;
