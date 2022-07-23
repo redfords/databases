d2d2fae4edcbdca60d04b6663dd1e10d0acad30d
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-23 01:52:09 -0300

    Add Find all wineries

--- StrataScratch/medium.sql
+++ StrataScratch/medium.sql
@@ -86,3 +86,29 @@ group by
   song_name
 order by year_rank
 limit 10;
+
+/*
+Find all wineries which produce wines by possessing aromas of plum, cherry, rose, or hazelnut
+
+Find all wineries which produce wines by possessing aromas of plum, cherry, rose, or hazelnut. To make it more simple, look only for
+singular form of the mentioned aromas.
+Output unique winery values only.
+
+winemag_p1
+id: int
+country: varchar
+description: varchar
+designation: varchar
+points: int
+price: float
+province: varchar
+region_1: varchar
+region_2: varchar
+variety: varchar
+winery: varchar
+*/
+
+select
+  distinct winery
+from winemag_p1
+where description ~* '\y(plum|cherry|rose|hazelnut)\y';
