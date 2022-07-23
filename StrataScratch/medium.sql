96b77375ea8aa7b5a4049d015e9af3720b8b77ef
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-07-23 02:25:01 -0300

    Add Largest Olympics

--- StrataScratch/medium.sql
+++ StrataScratch/medium.sql
@@ -112,3 +112,44 @@ select
   distinct winery
 from winemag_p1
 where description ~* '\y(plum|cherry|rose|hazelnut)\y';
+
+/*
+Largest Olympics
+
+Find the Olympics with the highest number of athletes. The Olympics game is a combination of the year and the season, and is
+found in the 'games' column. Output the Olympics along with the corresponding number of athletes.
+
+olympics_athletes_events
+id: int
+name: varchar
+sex: varchar
+age: float
+height: float
+weight: datetime
+team: varchar
+noc: varchar
+games: varchar
+year: int
+season: varchar
+city: varchar
+sport: varchar
+event: varchar
+medal: varchar
+*/
+
+with games_athletes as (
+  select
+    games,
+    count(distinct id) as athletes_count
+  from olympics_athletes_events
+  group by games
+  )
+select
+  games,
+  athletes_count
+from games_athletes
+where athletes_count = (
+  select
+    max(athletes_count)
+  from games_athletes
+  );
