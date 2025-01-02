22a887d4521f226b07f18d9325184e407a066d2f
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-01-02 00:35:13 -0300

    Add new solutions to basic join

--- HackerRank/basic_join.sql
+++ HackerRank/basic_join.sql
@@ -1,11 +1,39 @@
+/* Population Census
+https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true */
+
+select
+    sum(city.population) as sum_population
+from
+    city
+    inner join country on city.countrycode = country.code
+where
+    country.continent = 'Asia';
+
+/* African Cities
+https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true */
+
+select
+    city.name
+from
+    city
+    inner join country on city.countrycode = country.code
+where
+    country.continent = 'Africa';
+
+/* Average Population of Each Continent
+https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true */
+
+select
+    country.continent,
+    floor(avg(city.population)) as avg_city_population
+from
+    country
+    inner join city on country.code = city.countrycode
+group by
+    country.continent;
+
 /* The Report
-Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty
-doesn't want the NAMES of those students who received a grade lower than 8. The report must be in
-descending order by grade -- i.e. higher grades are entered first. If there is more than one student
-with the same grade (8-10) assigned to them, order those particular students by their name
-alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their
-grades in descending order. If there is more than one student with the same grade (1-7) assigned to
-them, order those particular students by their marks in ascending order. */
+*/
 
 select name, grade, marks
 from students a
@@ -26,11 +54,7 @@ join Grades g on s.Marks between g.Min_Mark and g.Max_Mark
 order by g.Grade desc, s.Name, s.Marks
 
 /* Top Competitors
-Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard!
-Write a query to print the respective hacker_id and name of hackers who achieved full scores for more
-than one challenge. Order your output in descending order by the total number of challenges in which
-the hacker earned a full score. If more than one hacker received full scores in same number of
-challenges, then sort them by ascending hacker_id. */
+. */
 
 select h.hacker_id, h.name
 from submissions s
@@ -43,11 +67,7 @@ having count(*) > 1
 order by count(*)desc, h.hacker_id
 
 /* Ollivander's Inventory
-Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
-Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to
-buy each non-evil wand of high power and age. Write a query to print the id, age, coins_needed, and
-power of the wands that Ron's interested in, sorted in order of descending power. If more than one wand
-has same power, sort the result in order of descending age.*/
+*/
 
 select id, age, m.coins_needed, m.power
 from
@@ -62,11 +82,7 @@ where wp.is_evil = 0
 order by m.power desc, age desc
 
 /* Challenges
-Julia asked her students to create some coding challenges. Write a query to print the hacker_id, name,
-and the total number of challenges created by each student. Sort your results by the total number of
-challenges in descending order. If more than one student created the same number of challenges, then
-sort the result by hacker_id. If more than one student created the same number of challenges and the
-count is less than the maximum number of challenges created, then exclude those students. */
+*/
 
 select c.hacker_id, h.name, count(c.challenge_id) as challenges_created
 from Hackers h
@@ -87,11 +103,7 @@ or challenges_created not in
 order by challenges_created desc, c.hacker_id
 
 /* Contest Leaderboard
-You did such a great job helping Julia with her last coding contest challenge that she wants you to work
-on this one, too! The total score of a hacker is the sum of their maximum scores for all of the
-challenges. Write a query to print the hacker_id, name, and total score of the hackers ordered by the
-descending score. If more than one hacker achieved the same total score, then sort the result by
-ascending hacker_id. Exclude all hackers with a total score of  from your result. */
+*/
 
 select m.hacker_id, h.name, sum(m.score) as total_score
 from (select hacker_id, challenge_id, max(score) as score
@@ -105,9 +117,7 @@ order by total_score desc, h.hacker_id
 
 /*
 Cities With More Customers Than Average
-Write a query which returns all cities with more customers than the average number of customers of all
-cities. For each such city, return the country name, the city name and the number of customers. Order
-the result by country name ascending.
+
 */
 
 select country_name, city_name, count(customer.id)
