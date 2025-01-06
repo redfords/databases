abdf195439d8ef29099687872198488ef3e37c6c
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-01-06 14:59:19 -0300

    Add New Companies to advanced select

--- HackerRank/advanced_select.sql
+++ HackerRank/advanced_select.sql
@@ -1,10 +1,5 @@
 /*  Type of Triangle
-Write a query identifying the type of each record in the TRIANGLES table using its three side lengths.
-Output one of the following statements for each record in the table:
-Equilateral: It's a triangle with 3 sides of equal length.
-Isosceles: It's a triangle with 2 sides of equal length.
-Scalene: It's a triangle with 3 sides of differing lengths.
-Not A Triangle: The given values of A, B, and C don't form a triangle. */
+https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true */
 
 select
 if(a + b > c and a + c > b and b + c > a,
@@ -16,23 +11,22 @@ from triangles
 
 
 /* The PADS
-Generate the following two result sets:
-1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first
-letter of each profession as a parenthetical (i.e.: enclosed in parentheses).
-2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending
-order, and output them in the following format:
-There are a total of [occupation_count] [occupation]s.
-where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation]
-is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they
-should be ordered alphabetically.*/
+https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true */
 
-select concat(name, '(', left(occupation, 1), ')')
+select
+    concat(name, '(', substring(occupation, 1, 1), ')') as name_occ
 from occupations
 order by name;
-select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.')
+select
+    concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') as str_occ
 from occupations
 group by occupation
-order by count(occupation), occupation
+order by count(occupation);
+
+/* New Companies
+https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true */
+
+
 
 /*
 Weather Department Statistics
