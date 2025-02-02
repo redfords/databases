070ab1e1a31b3c7e75f066c2d49ac24b654e71cf
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-02-02 00:36:01 -0300

    Update Exchange Seats

--- LeetCode/medium.sql
+++ LeetCode/medium.sql
@@ -136,32 +136,7 @@ select
 from Accounts
 	
 /* Exchange Seats
-Mary is a teacher in a middle school and she has a table seat storing students' names and their
-corresponding seat ids. The column id is continuous increment. Mary wants to change seats for the
-adjacent students.
-
-+---------+---------+
-|    id   | student |
-+---------+---------+
-|    1    | Abbot   |
-|    2    | Doris   |
-|    3    | Emerson |
-|    4    | Green   |
-|    5    | Jeames  |
-+---------+---------+
-
-For the sample input, the output is:
-+---------+---------+
-|    id   | student |
-+---------+---------+
-|    1    | Doris   |
-|    2    | Abbot   |
-|    3    | Green   |
-|    4    | Emerson |
-|    5    | Jeames  |
-+---------+---------+
-
-Note: If the number of students is odd, there is no need to change the last one's seat. */
+https://leetcode.com/problems/exchange-seats/description/ */
 
 select
 case
