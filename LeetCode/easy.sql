897c122c2195a5e173afbd020d1b6aee7db107a8
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-02-07 22:49:25 -0300

    Add Reformat Department Table

--- LeetCode/easy.sql
+++ LeetCode/easy.sql
@@ -300,6 +300,26 @@ from person p1, person p2
 where p1.email = p2.email
 and p1.id > p2.id
 
+# 1179. Reformat Department Table
+# https://leetcode.com/problems/reformat-department-table/description/
+    
+select 
+    id,
+    sum(case when month = 'Jan' then revenue end) as Jan_revenue,
+    sum(case when month = 'Feb' then revenue end) as Feb_revenue,
+    sum(case when month = 'Mar' then revenue end) as Mar_revenue,
+    sum(case when month = 'Apr' then revenue end) as Apr_revenue,
+    sum(case when month = 'May' then revenue end) as May_revenue,
+    sum(case when month = 'Jun' then revenue end) as Jun_revenue,
+    sum(case when month = 'Jul' then revenue end) as Jul_revenue,
+    sum(case when month = 'Aug' then revenue end) as Aug_revenue,
+    sum(case when month = 'Sep' then revenue end) as Sep_revenue,
+    sum(case when month = 'Oct' then revenue end) as Oct_revenue,
+    sum(case when month = 'Nov' then revenue end) as Nov_revenue,
+    sum(case when month = 'Dec' then revenue end) as Dec_revenue
+    from department
+    group by id
+    
 /* 1484. Group Sold Products By The Date
 https://leetcode.com/problems/group-sold-products-by-the-date/description/ */
 
