a3acbe8f1ca02eaf535974b7aa87d5d7546c90ec
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-02-08 00:05:33 -0300

    Update medium.sql

--- LeetCode/medium.sql
+++ LeetCode/medium.sql
@@ -22,6 +22,15 @@ from
     left join confirmations c on s.user_id = c.user_id
 group by user_id
 
+# 608. Tree Node
+# https://leetcode.com/problems/tree-node/description/
+	
+select id,
+case when p_id is NULL then 'Root'
+when id not in (select p_id from tree where p_id is not NULL) then 'Leaf'
+else 'Inner' end as type
+from tree
+
 /* Monthly Transactions I
 https://leetcode.com/problems/monthly-transactions-i/description/ */
 
