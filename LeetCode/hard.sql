c76c6af27c5d11719b4edd3b0472fbd4ea7fb41e
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-02-09 23:43:12 -0300

    Update hard.sql

--- LeetCode/hard.sql
+++ LeetCode/hard.sql
@@ -16,8 +16,28 @@ from
     inner join department d on d.id = e.departmentid
     inner join highest_salary h on e.salary = h.salary
         and e.departmentid = h.departmentid
-        and row_num <= 3
+        and row_num <= 3;
 
 # 601. Human Traffic of Stadium
 # https://leetcode.com/problems/human-traffic-of-stadium/description/
+
+with
+  stadium_group as (
+    select
+      id,
+      visit_date,
+      people,
+      id - row_number() over(order by id) as group_id
+    from Stadium
+    where people >= 100
+  )
+select id, visit_date, people
+from stadium_group
+where group_id in (
+    select group_id
+    from stadium_group
+    group by group_id
+    having count(*) >= 3
+  )
+order by visit_date;
 
